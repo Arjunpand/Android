5372dafbad9de5259566a9550ef6b55a89c80983
Arjunpand <arjunpain777@gmail.com>
2023-08-29 08:36:40 +0530

    -initial commit

---
+++ app/src/main/java/com/example/register1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.register1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
+
+    }
+
+
+
+}
